3fd0ccf0adb9e5b1bda9ae52a697083f5f3ab3ab
young <young_xu@qq.com>
2017-09-14 17:41:56 +0800

    delete ppt

--- src/public/content/catelog.js
+++ src/public/content/catelog.js
@@ -10,22 +10,22 @@ const gitTopicsList = [
       'https://github.com/xugy0926/getting-started-with-javascript/blob/master/topics/Win-Command-list.md'
   },
   {
-    title: '【课前预习】第一次使用Git',
+    title: '第一次使用Git',
     url:
       'https://github.com/xugy0926/getting-started-with-javascript/blob/master/topics/%E7%AC%AC%E4%B8%80%E6%AC%A1%E4%BD%BF%E7%94%A8Git.md'
   },
   {
-    title: '【课前预习】第一次pull request',
+    title: '第一次pull request',
     url:
       'https://github.com/xugy0926/getting-started-with-javascript/blob/master/topics/%E7%AC%AC%E4%B8%80%E6%AC%A1pull-request.md'
   },
   {
-    title: '【课前预习】第一次pull request错误汇总',
+    title: '第一次pull request错误汇总',
     url:
       'https://github.com/xugy0926/getting-started-with-javascript/blob/master/topics/%E7%AC%AC%E4%B8%80%E6%AC%A1pull-request%E9%94%99%E8%AF%AF%E6%B1%87%E6%80%BB.md'
   },
   {
-    title: '如何更新从主项目fork过来的项目@右张丽娜同学贡献',
+    title: '如何更新从主项目fork过来的项目@由张丽娜同学贡献',
     url:
       'https://github.com/xugy0926/getting-started-with-javascript/blob/master/topics/%E5%A6%82%E4%BD%95%E4%BB%8E%E4%B8%BB%E9%A1%B9%E7%9B%AE%E6%9B%B4%E6%96%B0fork%E7%9A%84%E9%A1%B9%E7%9B%AE.md'
   },
@@ -41,39 +41,6 @@ const gitTopicsList = [
   }
 ];
 
-const pptList = [
-  {
-    title: '第一节课PPT',
-    url:
-      'https://github.com/xugy0926/getting-started-with-javascript/blob/master/ppt/JavaScript%E7%BC%96%E7%A8%8B%E5%85%A5%E9%97%A8_%E7%AC%AC%E4%B8%80%E8%8A%82.pdf'
-  },
-  {
-    title: '第二节课PPT',
-    url:
-      'https://github.com/xugy0926/getting-started-with-javascript/blob/master/ppt/JavaScript%E7%BC%96%E7%A8%8B%E5%85%A5%E9%97%A8_%E7%AC%AC%E4%BA%8C%E8%8A%82.pdf'
-  },
-  {
-    title: '第三节课PPT',
-    url:
-      'https://github.com/xugy0926/getting-started-with-javascript/blob/master/ppt/JavaScript%E7%BC%96%E7%A8%8B%E5%85%A5%E9%97%A8_%E7%AC%AC%E4%B8%89%E8%8A%82.pdf'
-  },
-  {
-    title: '第四节课PPT',
-    url:
-      'https://github.com/xugy0926/getting-started-with-javascript/blob/master/ppt/JavaScript%E7%BC%96%E7%A8%8B%E5%85%A5%E9%97%A8_%E7%AC%AC%E5%9B%9B%E8%AF%BE.pdf'
-  },
-  {
-    title: '第五节课PPT',
-    url:
-      'https://github.com/xugy0926/getting-started-with-javascript/blob/master/ppt/JavaScript%E7%BC%96%E7%A8%8B%E5%85%A5%E9%97%A8_%E7%AC%AC%E4%BA%94%E8%8A%82.pdf'
-  },
-  {
-    title: '第六节课PPT',
-    url:
-      'https://github.com/xugy0926/getting-started-with-javascript/blob/master/ppt/JavaScript%E7%BC%96%E7%A8%8B%E5%85%A5%E9%97%A8_%E7%AC%AC%E5%85%AD%E8%AF%BE.pdf'
-  }
-];
-
 const topicsList = [
   {
     title: '第一次使用JSON',
@@ -81,7 +48,7 @@ const topicsList = [
       'https://github.com/xugy0926/getting-started-with-javascript/blob/master/topics/%E7%AC%AC%E4%B8%80%E6%AC%A1%E4%BD%BF%E7%94%A8JSON.md'
   },
   {
-    title: '【课前准备】安装NVM',
+    title: '安装NVM',
     url:
       'https://github.com/xugy0926/getting-started-with-javascript/blob/master/topics/%E5%AE%89%E8%A3%85nvm.md'
   },
@@ -90,19 +57,23 @@ const topicsList = [
     url: 'https://github.com/xugy0926/getting-started-with-javascript/blob/master/topics/%E5%A6%82%E4%BD%95%E5%9C%A8%E7%BB%88%E7%AB%AF%E6%89%A7%E8%A1%8C%E4%BB%A3%E7%A0%81.md'
   },
   {
-    title: '【第二节知识点】定义变量',
+    title: '定义变量',
     url:
       'https://github.com/xugy0926/getting-started-with-javascript/blob/master/topics/%E5%AE%9A%E4%B9%89%E5%8F%98%E9%87%8F.md'
   },
   {
-    title: '【第三节知识点】js编程重要语句',
+    title: 'js编程重要语句',
     url:
       'https://github.com/xugy0926/getting-started-with-javascript/blob/master/topics/js%E9%87%8D%E8%A6%81%E8%AF%AD%E5%8F%A5.md'
   },
   {
-    title: '【第三节知识点】变量命名技巧',
+    title: '变量命名技巧',
     url:
       'https://github.com/xugy0926/getting-started-with-javascript/blob/master/topics/%E5%8F%98%E9%87%8F%E5%91%BD%E5%90%8D%E7%AE%80%E6%98%8E%E6%8A%80%E5%B7%A7.md'
+  },
+  {
+    title: '语法学习指引',
+    url: 'https://github.com/xugy0926/getting-started-with-javascript/blob/master/topics/JavaScript%E7%9A%84%E8%AF%AD%E6%B3%95%E5%AD%A6%E4%B9%A0%E6%8C%87%E5%BC%95.md'
   },
   {
     title: '同步函数的两种返回结果',
@@ -124,6 +95,10 @@ const topicsList = [
     url: 'https://github.com/xugy0926/getting-started-with-javascript/blob/master/topics/%E4%B8%A4%E7%A7%8D%E5%AE%9A%E4%B9%89%E5%87%BD%E6%95%B0%E6%96%B9%E5%BC%8F%E7%9A%84%E5%B7%AE%E5%BC%82.md'
   },
   {
+    title: '匿名函数',
+    url: 'https://github.com/xugy0926/getting-started-with-javascript/blob/master/topics/%E5%8C%BF%E5%90%8D%E5%87%BD%E6%95%B0%E4%BD%BF%E7%94%A8%E6%8A%80%E5%B7%A7.md'
+  },
+  {
     title: '函数 - 定义完就执行',
     url: 'https://github.com/xugy0926/getting-started-with-javascript/blob/master/topics/%E5%87%BD%E6%95%B0%20-%20%E5%AE%9A%E4%B9%89%E5%AE%8C%E5%B0%B1%E6%89%A7%E8%A1%8C.md'
   },
@@ -132,7 +107,7 @@ const topicsList = [
     url: 'https://github.com/xugy0926/getting-started-with-javascript/blob/master/topics/callback%E5%87%BD%E6%95%B0%E7%9A%84%E8%AE%BE%E8%AE%A1%E5%AD%A6.md'
   },
   {
-    title: '[第9节课作业需要]了不起的axios(http)',
+    title: '了不起的axios(http)',
     url: 'https://github.com/xugy0926/getting-started-with-javascript/blob/master/topics/%E4%BA%86%E4%B8%8D%E8%B5%B7%E7%9A%84axios(http).md'
   },
   {
@@ -141,4 +116,4 @@ const topicsList = [
   }
 ];
 
-module.exports = { gitTopicsList, pptList, topicsList };
+module.exports = { gitTopicsList, topicsList };
